adfd03caa1229aed0ed544481ea0451c171fce43
Bruno Bernardo <bigsby@live.com>
2017-11-09 16:59:03 +0000

    Typos and clean up

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ An operating System created from scratch.
 # Prerequisites
 - **NASM** from [here](http://www.nasm.us/) > Assembler to compile *.asm* file to machine code~.
 - **QEmu** from [here](https://www.qemu.org/) > Emulator of physical machine. Also creates *.vdi* (VirtualBox disk files) from raw disk files.
-- **asdf** from [here](http://mingw-w64.org/doku.php/download) > C and C++ compilers.
+- **MinGW-w64** from [here](http://mingw-w64.org/doku.php/download) > C and C++ compilers.
 
 ## Helper Tools
 - **GnuWin32 CoreUtils** from [here](http://gnuwin32.sourceforge.net/packages/coreutils.htm) > For tools like *od* (Octal dump).
 - **HxD** from [here](https://mh-nexus.de/en/downloads.php?product=HxD) > Raw hexadecimal file editor.
 - **VirtualBox** from [here](https://www.virtualbox.org/) > Virtual Machine hypervisor and manager.
 
+# v0.0.2
+- Use function to write string
+- 
 # v0.0.1
 - Initial version
 - Manually writting message
-
-# v0.0.2
-- Use function to write string
--- src/bigOS-0.0.2.asm
+++ src/bigOS-0.0.2.asm
@@ -1,12 +1,12 @@
 ; 
 ; A simple boot sector that prints a message to the screen using a BIOS routine. 
 ;
-[org 0x7c00]            ; instruct loader where the code is loaded
+[org 0x7c00]                        ; instruct loader where the code is loaded
 
 mov bx, WELCOME_MESSAGE
 call print_string
 
-jmp $                   ; Jump to the current address (i.e. forever).
+jmp $                               ; Jump to the current address (i.e. forever).
 
 WELCOME_MESSAGE:
     db "BigOS is up and running!", 0
@@ -14,24 +14,24 @@ WELCOME_MESSAGE:
 print_string:
     pusha
 
-    mov ah, 0x0e        ; set scrolling teletype
+    mov ah, 0x0e                    ; set scrolling teletype
 
     start_lopp:
 
-        mov al, [bx]    ; set al to the value in bx address
+        mov al, [bx]                ; set al to the value in bx address
         
-        cmp al, 0       ; compare the value al address to 0 to test for end of string
-        je return       ; if it is 0, exit
+        cmp al, 0                   ; compare the value al address to 0 to test for end of string
+        je print_string_return      ; if it is 0, exit
                 
-        int 0x10        ; call BIOS interrupt
+        int 0x10                    ; call BIOS interrupt
         
-        add bx, word 1  ; move to next bx address
-        jmp start_lopp  ; jump to start of loop
+        add bx, word 1              ; move to next bx address
+        jmp start_lopp              ; jump to start of loop
 
-    return:
-    mov al, 13          ; carriage return
+    print_string_return:
+    mov al, 13                      ; carriage return
     int 0x10
-    mov al, 10          ; new line
+    mov al, 10                      ; new line
     int 0x10
 
     popa
@@ -40,7 +40,7 @@ print_string:
 ; Padding and magic BIOS number. 
 ;
 
-times 510-($-$$) db 0   ; Pad the boot sector out with zeros
+times 510-($-$$) db 0               ; Pad the boot sector out with zeros
 
-dw 0xaa55               ; Last two bytes form the magic number , 
-                        ; so BIOS knows we are a boot sector.
+dw 0xaa55                           ; Last two bytes form the magic number , 
+                                    ; so BIOS knows we are a boot sector.
